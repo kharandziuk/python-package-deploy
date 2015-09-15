de7f77f60b6b8a19d266de6419373a24279b853a
Max Kharandziuk <max.kharandziuk@gmail.com>
2015-09-15 13:12:43 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='my_project',
     version='0.1.0',
-    packages=['my_project'],
+    packages=['my_package'],
     entry_points={
       'console_scripts': [
-        'my_project = my_project.__main__:main'
+        'my_package = my_package.__main__:main'
       ]
     },
 )
